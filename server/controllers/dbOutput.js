dc2fb560e5a35c524afa3d9cabbe9d5370b4ac3e
Junda Zhang <zhangjunda23@gmail.com>
2018-08-07 17:22:35 +0800

    解决too many connections的问题
    改为框架推荐的连接数据库的方式

--- server/controllers/dbOutput.js
+++ server/controllers/dbOutput.js
@@ -2,8 +2,10 @@ const {
   mapMysql: config
 } = require('../config')
 
+const { mysql } = require('../qcloud')
+
 module.exports = async(ctx, next) => {
-  const knex = require('knex')({
+  /*const knex = require('knex')({
     client: 'mysql',
     connection: {
       host: config.host,
@@ -14,29 +16,32 @@ module.exports = async(ctx, next) => {
       charset: config.char,
       multipleStatements: true
     }
-  })
+  })*/
 
   //分三种情况：1、读取全部管理员列表；2、读取全部有效的义工列表；3、读取特定WID的有效的义工列表
   var flag = ctx.request.body.flag
   if (flag === 0) //获取全部管理员列表
   {
-    var res = await knex('t_ManagerList').select();
-    ctx.state.code = 200;
-    ctx.state.data = res;
+    await mysql('t_ManagerList').select('*').then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
   } else if (flag === 1) //获取全部有效的义工列表
   {
-    var res = await knex('t_VolunteerList').select().where('Valid',1);
-    ctx.state.code = 200;
-    ctx.state.data = res;
+    await mysql('t_VolunteerList').select('*').where('Valid', 1).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
 
   } else if (flag === 2) //获取特定WID的有效的义工列表
   {
     var WID = ctx.request.body.WID;
-    var res = await knex('t_VolunteerList').where({Valid:1,WID:WID});
-    ctx.state.code = 200;
-    ctx.state.data = res;
-  } else {
+    await mysql('t_VolunteerList').where({ Valid: 1, WID: WID }).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;
+      });
 
+  } else {
+    ctx.state.code = -1;
+    ctx.state.data = 'flag错误';
   }
 
 
--- server/controllers/dbUpdate.js
+++ server/controllers/dbUpdate.js
@@ -2,8 +2,10 @@ const {
   mapMysql: config
 } = require('../config')
 
+const { mysql } = require('../qcloud')
+
 module.exports = async(ctx, next) => {
-  const knex = require('knex')({
+  /*const knex = require('knex')({
     client: 'mysql',
     connection: {
       host: config.host,
@@ -14,46 +16,46 @@ module.exports = async(ctx, next) => {
       charset: config.char,
       multipleStatements: true
     }
-  })
+  })*/
 
   //分三种情况：1、删除指定WID的数据，数据库中仅把Valid设置为0；2、更新指定WID的用户数据；3、写入新的一条数据；4、更改义工状态（正在帮忙、待命、帮不上忙）
   var flag = ctx.request.body.flag
   if (flag === 0) //删除指定WID的数据，数据库中仅把Valid设置为0
   {
     let WID = ctx.request.body.WID;
-    var res = await knex('t_VolunteerList').where({
+    await mysql('t_VolunteerList').where({
       Valid: 1,
       WID: WID
-    }).update('Valid', 0);
-    ctx.state.code = 200;
-    ctx.state.data = res;
+    }).update('Valid', 0).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
   } else if (flag === 1) //更新指定WID的用户数据
   {
     let WID = ctx.request.body.WID;
     let UserInfo = ctx.request.body.UserInfo;
-    var res = await knex('t_VolunteerList').where({
+    await mysql('t_VolunteerList').where({
       Valid: 1,
       WID: WID
-    }).update(UserInfo);
-    ctx.state.code = 200;
-    ctx.state.data = res;
-
+    }).update(UserInfo).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
   } else if (flag === 2) //写入新的一条数据
   {
     let UserInfo = ctx.request.body.UserInfo;
-    var res = await knex('t_VolunteerList').insert(UserInfo);
-    ctx.state.code = 200;
-    ctx.state.data = res;
+    await mysql('t_VolunteerList').insert(UserInfo).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
   } else if (flag === 3) //更改义工状态（正在帮忙、待命、帮不上忙）
   {
     let WID = ctx.request.body.WID;
     let Status = ctx.request.body.Status;
-    var res = await knex('t_VolunteerList').where({
+    await mysql('t_VolunteerList').where({
       Valid: 1,
       WID: WID
-    }).update('Status', Status);
-    ctx.state.code = 200;
-    ctx.state.data = res;
+    }).update('Status', Status).then(res => {
+      ctx.state.code = 200;
+      ctx.state.data = res;});
+
   } else {
     ctx.state.code = -1;
     ctx.state.data = 'flag错误';
