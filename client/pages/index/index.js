05b3e2971474e7f832ac9071ace176adf569380a
watsonLidaye <610466535@qq.com>
2018-08-08 21:59:16 +0800

    优化首页管理员判断

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -31,23 +31,25 @@ Page({
     let nickName = e.detail.userInfo.nickName
     let user_list = manger_list
     let path = ""
-    for (let i in user_list) {
-      if (user_list[i].UserName == nickName) {
-        jumpStauts = 1
-      }
-    }
+ 
     if (e.detail.userInfo.nickName) {
-      switch (jumpStauts) {
-        case 0:
-          path = "/pages/helper/index?username=" + nickName
-          break;
-        case 1:
-          path = "/pages/organizer/index?"
-          break;
-      }
+     
       wx.login({
         success: function(res) {
           m_DB.GetOpenID(res.code, res => {
+            for (let i in user_list) {
+              if (user_list[i].WID == res.openid) {
+                jumpStauts = 1
+              }
+            }
+            switch (jumpStauts) {
+              case 0:
+                path = "/pages/helper/index?username=" + nickName
+                break;
+              case 1:
+                path = "/pages/organizer/index?"
+                break;
+            }
             wx.navigateTo({
               url: path + '&openid=' + res.openid,
             })
